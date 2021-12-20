4d35e82ef088b98fa9b3de4c5a16a8b6d50dcb8b
xiongtao <xiongtao@txslgames.com>
2021-12-20 15:01:23 +0800

    加入sub_service

---
+++ frame/sub_service.go
@@ -0,0 +1,39 @@
+package frame
+
+import (
+	"sync"
+)
+
+type SubService struct {
+	loop      *Loop
+	loopReply *Loop
+	wgClose   sync.WaitGroup
+}
+
+func NewSubService(loopSize int, loopFullWarn bool, loopReply *Loop) *SubService {
+	return &SubService{
+		loop:      NewLoop(loopSize, loopFullWarn),
+		loopReply: loopReply,
+	}
+}
+
+func (ss *SubService) Call(call LoopFun) {
+	ss.loop.Post(call)
+}
+
+func (ss *SubService) Reply(reply LoopFun) {
+	ss.loopReply.Post(reply)
+}
+
+func (ss *SubService) Run() {
+	ss.wgClose.Add(1)
+	go func() {
+		ss.wgClose.Done()
+		ss.loop.Run()
+	}()
+}
+
+func (ss *SubService) Close(waitHandle bool) {
+	ss.loop.Close(waitHandle)
+	ss.wgClose.Wait()
+}
